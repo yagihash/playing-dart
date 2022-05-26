ff2159af48dea7b53a6ace50b46358c7b4f1d75d
Yu YAGIHASHI <yagihash@mercari.com>
2022-05-26 18:21:17 +0900

    first commit

---
+++ bin/playing_dart.dart
@@ -0,0 +1,5 @@
+import 'package:playing_dart/playing_dart.dart' as playing_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${playing_dart.calculate()}!');
+}
---
+++ test/playing_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:playing_dart/playing_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
